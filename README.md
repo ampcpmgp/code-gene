261572e84d4fa081fd5977bd622879aaccb8ac03
teruzoufox <satoshi.terahara@chegilon.com>
2019-03-20 03:17:27 +0900

    Modify パッケージ化対応

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ $ npm install electron@1.6.1 --save-dev
 ```bash:
 $ ./node_modules/.bin/electron .
 ```
+
+## パッケージ化
+```bash:
+# パッケージマネージャインストール
+$ npm install electron-packager@8.5.2 --save-dev
+# パッケージ化実行
+$ ./node_modules/.bin/electron-packager . code-generator --platform=darwin,win32 --arch=x64 --electronVersion=0.36.1
+```
--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 let win;
 function createWindow() {
     win = new BrowserWindow({ width: 800, height: 660 });
     win.loadURL(`file://${__dirname}/index.html`);
     win.on("closed", () => { win = null; });
+
+    var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+    
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
